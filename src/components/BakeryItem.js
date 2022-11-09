95b720cff103dad760a19e9214ac37e9ea15618f
bgriswol <71152751+bgriswol@users.noreply.github.com>
2022-11-09 12:08:18 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,20 @@
 // TODO: create a component that displays a single bakery item
+function BakeryItem(props) {
+    const handleClick= () => {
+        var currList = props.cart
+        console.log(currList, "beginning list")
+        var newItem = props.info.name
+        console.log(props.cart, "current cart")
+        props.setCart([...props.cart, newItem]);
+    }
+    return <div>
+        <h2>{props.info.name}</h2>
+        <h3>{props.info.description}</h3>
+        <h3 style={{color:"blue"}}>{props.info.price}</h3>
+        <img src = {props.info.image} width="50%" height="50%"/>
+        <button onClick={handleClick}> Add to Cart </button>
+        </div>;
+  }
+
+export default BakeryItem
+
